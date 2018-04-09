df0c8cffc317a35c56849148501afd89dfc63c60
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2018-04-08 22:05:49 -0500

    class IntNode
    creating class node and creating a node in the main

--- LinkedLists/main.cpp
+++ LinkedLists/main.cpp
@@ -1,8 +1,22 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+class IntNode {
+public:
+	int info;
+	IntNode *next;
+	IntNode() {
+		next = 0;
+	}
 
+	IntNode(int i, IntNode *in = 0) {
+		info = i;
+		next = in;
+	}
+};
+
+int main() {
+	IntNode *p = new IntNode(10);
 	system("pause");
 	return 0;
 }
