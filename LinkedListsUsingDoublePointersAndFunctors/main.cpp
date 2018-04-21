9929f864d108b0298fadb7d6cda87fddd7a95d76
Fabrizio <fabrizio.flores@ucsp.edu.pe>
2018-04-20 20:49:44 -0500

    corrections

--- LinkedListsUsingDoublePointersAndFunctors/main.cpp
+++ LinkedListsUsingDoublePointersAndFunctors/main.cpp
@@ -11,41 +11,45 @@ struct CNode{
 	}
 };
 
-template <class T, class Op>
+template <class T>
 class CList {
 	public:
 		CNode<T> *m_head;
-		CList() { 
-			m_head = 0; 
+		CList( T x) { 
+			m_head = new CNode<T>(x); 
 		}
-		Op m_op;
 		bool find(T x, CNode<T>**& p) {
-			cout << "&m_head: " << &m_head << endl;
+			//cout << "&m_head: " << &m_head << endl;
 
 			p = &m_head;
-			cout << "*p: " << *p << endl;
-			cout << "(*p): " << (*p) << endl;
+			// << "*p: " << *p << endl;
+			//cout << "(*p)->data: " << (*p)->data << endl;
+			//cout << "x: " << x << endl;
 			while (*p != NULL && (*p)->data < x)
 			{
 				p = &(*p)->m_next;
 			}
+			//cout << "(*p)->data: " << (*p)->data << endl;
+			//cout << "x: " << x << endl;
 			return (*p) != NULL && (*p)->data == x;
 		}
 		bool Insert(T x) {
 			CNode<T>**p;
-			if (find(x, p))return 0;
+			if (find(x, p))return false;
 			CNode<T>*t = new CNode<T>(x);
 			t->m_next = *p;
 			*p = t;
-			return 1;
+			return true;
 		}
 		bool Remove(T x) {
 			CNode<T>**p;
-			if (find(x, p))return 0;
+			cout << "remover" << endl;
+			if (find(x, p))return false;
+			cout << "pasa el if" << endl;
 			CNode<T>*temp = *p;
 			*p = temp->m_next;
 			delete temp;
-			return 1;
+			return true;
 		}
 		void Print(){
 			cout << "--> ";
@@ -62,9 +66,33 @@ class CList {
 };
 
 int main() {
-	CList<int>*test = new CList<int>();
-	test->Insert(10);
-	test->Insert(11);
-	test->Print();
+	cout << "Ingrse un valor para inicializar la lista: ";
+	int tempo; cin >> tempo;
+	CList<int>test(tempo);
+	test.Print();
+	test.Insert(tempo);
+	test.Print();
+	test.Remove(tempo);
+	test.Print();
+	/*int hasta = 1;
+	while (hasta != 0) {
+		cout << "Desea : "<<endl;
+		cout << "1) Insertar" << endl;
+		cout << "2) Eliminar" << endl;
+		cout << "0) Terminar" << endl;
+		cout << "Ingrese el numero de opcion" << endl;
+		cin >> hasta;
+		if (hasta == 1) {
+			cout << "Ingrese el numero que desea insertar: ";
+			cin >> tempo;
+			test->Insert(tempo);
+		}
+		if (hasta == 2) {
+			cout << "Ingrese el numero que desea eliminar: ";
+			cin >> tempo;
+			test->Remove(tempo);
+		}
+		test->Print();
+	}*/
 	system("pause");
 }
